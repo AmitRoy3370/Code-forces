01f1e7945d70c31d81645c43e2234112284665ce
AmitRoy3370 <70816613+AmitRoy3370@users.noreply.github.com>
2021-05-14 08:39:48 +0600

    Add files via upload

---
+++ A_Minimal_Square.java
@@ -0,0 +1,87 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class A_Minimal_Square {
+
+       static Scanner in=new Scanner();
+    
+    static PrintWriter out=new PrintWriter(System.out);
+    
+    static int a,b,testCases;
+    
+    static void solve(){
+        
+        out.println( (int)Math.pow(Math.min( Math.max(2*a, b) , Math.max( 2*b,a ) ),2) );
+        out.flush();
+        
+    }
+    
+    public static void main(String[] amit) throws IOException {
+        
+        testCases=in.nextInt();
+        
+        for(int t=0;t<testCases;t++){
+            
+            a=in.nextInt();
+            b=in.nextInt();
+            
+            solve();
+            
+        }
+        
+    }
+    
+    static class Scanner{
+        
+        BufferedReader in;
+        StringTokenizer st;
+
+        public Scanner() {
+        
+            in=new BufferedReader( new InputStreamReader(System.in) );
+            
+        }
+        
+        String next() throws IOException{
+            
+            while(st==null || !st.hasMoreElements()){
+                
+                st=new StringTokenizer( in.readLine() );
+                
+            }
+            
+            return st.nextToken();
+            
+        }
+        
+        String nextLine() throws IOException{
+            
+            return in.readLine();
+            
+        }
+        
+        int nextInt() throws IOException{
+            
+            return Integer.parseInt(next());
+            
+        }
+        
+        double nextDouble() throws IOException{
+            
+            return Double.parseDouble( next() );
+            
+        }
+        
+        long nextLong() throws IOException{
+            
+            return Long.parseLong(next());
+            
+        }
+        
+    }
+    
+}
