6fea3efe2a7d81b264a2e0cd8ffddce1a59a85ac
AmitRoy3370 <70816613+AmitRoy3370@users.noreply.github.com>
2021-06-04 18:54:26 +0600

    Add files via upload

---
+++ A_Candies_and_Two_Sisters.java
@@ -0,0 +1,73 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class A_Candies_and_Two_Sisters {
+
+      static Scanner in=new Scanner();
+    static PrintWriter out=new PrintWriter(System.out);
+    
+    static int testCases;
+    
+    static long n;
+    
+    public static void main(String[] amit) throws IOException {
+        
+        testCases=in.nextInt();
+        
+        for(int t=0;t<testCases;t++){
+            
+            out.println( (in.nextLong()-1)/2 );
+            out.flush();
+            
+        }
+        
+    }
+ 
+     static class Scanner{
+        
+        BufferedReader in;
+        StringTokenizer st;
+
+        public Scanner() {
+        
+            in=new BufferedReader( new InputStreamReader( System.in ) );
+            
+        }
+        
+        String next() throws IOException{
+            
+            while( st==null || !st.hasMoreElements() ){
+                
+                st=new StringTokenizer(in.readLine());
+                
+            }
+            
+            return st.nextToken();
+            
+        }
+        
+        String nextLine() throws IOException{
+            
+            return in.readLine();
+            
+        }
+        
+        int nextInt() throws IOException{
+            
+            return Integer.parseInt( next() );
+            
+        }
+        
+        long nextLong() throws IOException{
+            
+            return Long.parseLong( next() );
+            
+        }
+        
+    }
+    
+}
