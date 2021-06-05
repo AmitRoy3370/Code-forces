09582b235b8c1a794c51b1a9cbb641313164a12d
AmitRoy3370 <70816613+AmitRoy3370@users.noreply.github.com>
2021-06-05 20:08:25 +0600

    Add files via upload

---
+++ B_Vanya_and_Lanterns.java
@@ -0,0 +1,195 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class B_Vanya_and_Lanterns {
+
+      static Scanner in=new Scanner();
+    
+    static PrintWriter out=new PrintWriter(System.out);
+    
+    static int n,l;
+    
+    static double a[];
+    
+    static void merge(double a[],int left,int right,int mid){
+        
+        int n1=mid-left+1,n2=right-mid;
+        
+        double L[]=new double[n1];
+        double R[]=new double[n2];
+        
+        for(int i=0;i<n1;i++){
+            
+            L[i]=a[left+i];
+            
+        }
+        
+        for(int i=0;i<n2;i++){
+            
+            R[i]=a[mid+1+i];
+            
+        }
+        
+        int i=0,j=0,k=left;
+        
+        while(i<n1 && j<n2){
+            
+            if(L[i]<=R[j]){
+                
+                a[k]=L[i];
+                
+                i++;
+                
+            }else{
+                
+                a[k]=R[j];
+                
+                j++;
+                
+            }
+            
+            k++;
+            
+        }
+        
+        while(i<n1){
+            
+        a[k]=L[i];
+        
+        i++;
+        k++;
+            
+        }
+        
+        while(j<n2){
+            
+            a[k]=R[j];
+            
+            j++;
+            k++;
+            
+        }
+        
+    }
+    
+    static void sort(double a[],int left,int right){
+        
+        if(left>=right){
+            
+            return;
+            
+        }
+        
+        int mid=(left+right)/2;
+        
+        sort(a,left,mid);
+        sort(a,mid+1,right);
+        
+        merge(a,left,right,mid);
+        
+    }
+    
+    static void solve(){
+        
+        sort(a,0,n-1);
+        
+        double ans=Math.max(a[0], l-a[n-1])*2;
+        
+        for(int i=1;i<n;i++){
+            
+            ans=Math.max(a[i]-a[i-1],ans);
+            
+        }
+        
+        out.println(ans/2);
+        out.flush();
+        
+    }
+    
+    public static void main(String[] amit) throws IOException {
+        
+        n=in.nextInt();
+        
+        l=in.nextInt();
+        
+        a=new double[n];
+        
+        for(int i=0;i<n;i++){
+            
+            a[i]=in.nextDouble();
+            
+        }
+        
+        solve();
+        
+    }
+    
+      static class Scanner{
+        
+        BufferedReader in;
+        StringTokenizer st;
+
+        public Scanner() {
+            
+            in=new BufferedReader(new InputStreamReader(System.in));
+            
+        }
+        
+        String next() throws IOException{
+            
+            while(st==null || !st.hasMoreElements()){
+                
+                st=new StringTokenizer(in.readLine());
+                
+            }
+            
+            return st.nextToken();
+            
+        }
+        
+        String nextLine() throws IOException{
+            
+            return in.readLine();
+            
+        }
+        
+        int nextInt() throws IOException{
+            
+            return Integer.parseInt(next());
+            
+        }
+        
+        double nextDouble() throws IOException{
+            
+            return Double.parseDouble( next());
+            
+        }
+        
+        
+        long nextLong() throws IOException{
+            
+            return Long.parseLong(next());
+            
+        }
+    }
+    
+}
+/*
+
+7 15
+15 5 3 7 9 14 0
+
+
+*/
+
+/*
+
+2 5
+2 5
+
+
+*/
