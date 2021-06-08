a5c2e487a92f63c80c3a12b3dfc41ea24df5654f
AmitRoy3370 <70816613+AmitRoy3370@users.noreply.github.com>
2021-06-08 10:56:14 +0600

    Add files via upload

---
+++ A_EhAb_AnD_gCd.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class A_EhAb_AnD_gCd{
+
+static Scanner in=new Scanner(System.in);
+
+static int testCases,n;
+
+public static void main(String[] amit){
+
+testCases=in.nextInt();
+
+for(int t=0;t<testCases;t++){
+
+n=in.nextInt();
+
+System.out.println(1+" "+(n-1) );
+
+}
+
+}
+
+}
