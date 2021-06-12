b372e3dd91aa42ea9e5cfa4f3ceec32d06b83a8d
AmitRoy3370 <70816613+AmitRoy3370@users.noreply.github.com>
2021-06-12 09:17:33 +0600

    Add files via upload

---
+++ A_Prison_Break.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+
+public class A_Prison_Break {
+
+    public static void main(String[] amit) {
+        
+        Scanner in=new Scanner(System.in);
+        
+        int testCases=in.nextInt();
+        
+        for(int t=0;t<testCases;t++){
+            
+            System.out.println(in.nextInt()*in.nextInt());
+            
+        }
+        
+    }
+    
+}
