afaf0bb70f507e3dbd9ff9ad50f4cc4a1c033026
AmitRoy3370 <70816613+AmitRoy3370@users.noreply.github.com>
2021-06-14 13:45:46 +0600

    Add files via upload

---
+++ D_XOR_Game.java
@@ -0,0 +1,97 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class D_XOR_Game {
+
+      static Scanner in=new Scanner();
+    static PrintWriter out=new PrintWriter(System.out);
+    
+    static int testCases,a,b;
+    
+    public static void main(String[] amit) throws IOException {
+        
+        testCases=in.nextInt();
+        
+        for(int t=0;t<testCases;t++){
+            
+            a=in.nextInt();
+            b=in.nextInt();
+            
+            if((a^b)==a+b){
+                
+                out.println("NO");
+                out.flush();
+                
+            }else{
+                
+                out.println("YES");
+                out.flush();
+                
+            }
+            
+        }
+        
+    }
+    
+    static class Scanner{
+        
+        BufferedReader in;
+        StringTokenizer st;
+
+        public Scanner() {
+        
+            in=new BufferedReader(new InputStreamReader(System.in));
+            
+        }
+        
+        String next() throws IOException{
+            
+            while(st==null || !st.hasMoreElements()){
+                
+                st=new StringTokenizer(in.readLine());
+                
+            }
+            
+            return st.nextToken();
+            
+        }
+        
+        int nextInt() throws IOException{
+            
+            return Integer.parseInt(next());
+            
+        }
+        
+        long nextLong() throws IOException{
+            
+            return Long.parseLong(next());
+            
+        }
+        
+        double nextDouble() throws IOException{
+            
+            return Double.parseDouble(next());
+            
+        }
+        
+        String nextLine() throws IOException{
+            
+            return in.readLine();
+            
+        }
+    }    
+    
+}
+/*
+
+3
+2 2
+2 4
+6 10
+
+
+*/
