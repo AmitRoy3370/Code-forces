421de33a9b59a461f7c8fc68009df3c878b0d5bb
AmitRoy3370 <70816613+AmitRoy3370@users.noreply.github.com>
2021-06-17 12:20:38 +0600

    Add files via upload

---
+++ A_Required_Remainder.java
@@ -0,0 +1,108 @@
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class A_Required_Remainder {
+
+      static Scanner in=new Scanner();
+    
+    static PrintWriter out=new PrintWriter( new BufferedOutputStream(System.out) );
+    
+    static int testCases;
+    
+    static long n,x,y;
+    
+    public static void main(String[] amit) throws IOException {
+        
+        testCases=in.nextInt();
+        
+        for(int t=0;t<testCases;t++){
+            
+            x=in.nextLong();
+            
+            y=in.nextLong();
+            
+            n=in.nextLong();
+            
+            if( n-n%x+y<=n ){
+                
+                out.println((n-n%x+y));
+                out.flush();
+                
+            }else{
+                
+                out.println( (n-n%x+y-x ) );
+                out.flush();
+                
+            }
+            
+        }
+        
+    }
+    
+     static class Scanner{
+        
+        BufferedReader in;
+        StringTokenizer st;
+
+        public Scanner() {
+        
+            in=new BufferedReader(new InputStreamReader(System.in));
+            
+        }
+        
+        String next() throws IOException{
+            
+            while(st==null || !st.hasMoreElements()){
+                
+                st=new StringTokenizer(in.readLine());
+                
+            }
+            
+            return st.nextToken();
+            
+        }
+        
+        int nextInt() throws IOException{
+            
+            return Integer.parseInt(next());
+            
+        }
+        
+        long nextLong() throws IOException{
+            
+            return Long.parseLong(next());
+            
+        }
+        
+        double nextDouble() throws IOException{
+            
+            return Double.parseDouble(next());
+            
+        }
+        
+        String nextLine() throws IOException{
+            
+            return in.readLine();
+            
+        }
+    }    
+    
+}
+/*
+
+7
+7 5 12345
+5 0 4
+10 5 15
+17 8 54321
+499999993 9 1000000000
+10 5 187
+2 0 999999999
+
+
+*/
