344ccae233112cf525d22873ba439ba8c39163e0
AmitRoy3370 <70816613+AmitRoy3370@users.noreply.github.com>
2021-07-30 22:47:12 +0600

    Add files via upload

---
+++ A_PizzaForces.java
@@ -0,0 +1,145 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+
+public class A_PizzaForces {
+
+         static Scanner in=new Scanner();
+    
+    static PrintWriter out=new PrintWriter( new OutputStreamWriter(System.out) );
+
+    static int testCases;
+    
+    static long n;
+    
+    static void solve(){
+        
+        long ans=0;
+         
+     ans=(n*5)/2;
+     
+        out.println(ans);
+        out.flush();
+        
+    }
+    
+    public static void main(String[] amit) throws IOException {
+        
+        testCases=in.nextInt();
+        
+        for(int t=0;t<testCases;t++){
+            
+            n=in.nextLong();
+            
+            if( n<=6 ){
+                
+                out.println(15);
+                out.flush();
+                
+            }else{
+            
+                if(n%2!=0){
+                    
+                    n++;
+                    
+                }
+                
+            solve();
+            
+            }
+            
+        }
+        
+    }
+    
+      static class Scanner{
+        
+        BufferedReader in;
+        StringTokenizer st;
+
+        public Scanner() {
+        
+            in=new BufferedReader( new InputStreamReader(System.in) );
+            
+        }
+        
+        String next() throws IOException{
+            
+            while(st==null || !st.hasMoreElements()){
+                
+                st=new StringTokenizer(in.readLine());
+                
+            }
+            
+            return st.nextToken();
+            
+        }
+        
+        int nextInt() throws IOException{
+            
+            return Integer.parseInt(next());
+            
+        }
+        
+        long nextLong() throws IOException{
+            
+            return Long.parseLong(next());
+            
+        }
+        
+        String nextLine() throws IOException{
+            
+            return in.readLine();
+            
+        }
+        
+        char nextChar() throws IOException{
+            
+            return next().charAt(0);
+            
+        }
+        
+        double nextDouble() throws IOException{
+            
+            return Double.parseDouble(next());
+            
+        }
+        
+        float nextFloat() throws IOException{
+            
+            return Float.parseFloat(next());
+            
+        }
+        
+        boolean nextBoolean() throws IOException{
+            
+            return Boolean.parseBoolean(next());
+            
+        }
+        
+        void close() throws IOException{
+            
+            in.close();
+            
+        }
+        
+    }
+    
+}
+/*
+
+6
+12
+15
+300
+1
+9999999999999999
+3
+
+
+*/
