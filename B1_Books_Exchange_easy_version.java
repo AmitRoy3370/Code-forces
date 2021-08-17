c63d5c79c930527ce50c3357b53bcf3ed0e7d2d4
AmitRoy3370 <70816613+AmitRoy3370@users.noreply.github.com>
2021-08-17 08:24:04 +0600

    Add files via upload

---
+++ B1_Books_Exchange_easy_version.java
@@ -0,0 +1,156 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class B1_Books_Exchange_easy_version {
+
+        static Scanner in=new Scanner();
+    
+    static PrintWriter out=new PrintWriter( new OutputStreamWriter(System.out) );
+   
+    static int testCases,n;
+    
+    static int a[];
+    
+    static void solve(){
+        
+        for(int i=1;i<=n;i++){
+            
+            int j=a[i],count=1;
+            
+            while(i!=j){
+                
+                j=a[j];
+                
+                count++;
+                
+            }
+            
+            out.print(count+" " );
+            out.flush();
+            
+        }
+        
+    }
+    
+    public static void main(String[] amit) throws IOException {
+        
+        testCases=in.nextInt();
+        
+        for(int t=0;t<testCases;t++){
+            
+            n=in.nextInt();
+            
+            a=new int[n+1];
+            
+            for(int i=1;i<=n;i++){
+                
+                a[i]=in.nextInt();
+                
+            }
+            
+            solve();
+            
+            out.println();
+            out.flush();
+            
+        }
+        
+    }
+    
+     static class Scanner{
+        
+        BufferedReader in;
+        StringTokenizer st;
+
+        public Scanner() {
+        
+            in=new BufferedReader( new InputStreamReader(System.in) );
+            
+        }
+        
+        String next() throws IOException{
+            
+            while(st==null || !st.hasMoreElements()){
+                
+                st=new StringTokenizer(in.readLine());
+                
+            }
+            
+            return st.nextToken();
+            
+        }
+        
+        int nextInt() throws IOException{
+            
+            return Integer.parseInt(next());
+            
+        }
+        
+        long nextLong() throws IOException{
+            
+            return Long.parseLong(next());
+            
+        }
+        
+        String nextLine() throws IOException{
+            
+            return in.readLine();
+            
+        }
+        
+        char nextChar() throws IOException{
+            
+            return next().charAt(0);
+            
+        }
+        
+        double nextDouble() throws IOException{
+            
+            return Double.parseDouble(next());
+            
+        }
+        
+        float nextFloat() throws IOException{
+            
+            return Float.parseFloat(next());
+            
+        }
+        
+        boolean nextBoolean() throws IOException{
+            
+            return Boolean.parseBoolean(next());
+            
+        }
+        
+        void close() throws IOException{
+            
+            in.close();
+            
+        }
+        
+    }
+    
+}
+/*
+
+6
+5
+1 2 3 4 5
+3
+2 3 1
+6
+4 6 2 1 5 3
+1
+1
+4
+3 4 1 2
+5
+5 1 2 4 3
+
+
+*/
