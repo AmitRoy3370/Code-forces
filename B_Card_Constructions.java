31bab9302ddfb35513cc40f7442e569d60af9047
AmitRoy3370 <70816613+AmitRoy3370@users.noreply.github.com>
2021-08-30 16:53:03 +0600

    Add files via upload

---
+++ B_Card_Constructions.java
@@ -0,0 +1,134 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class B_Card_Constructions {
+
+     static Scanner in=new Scanner();
+    
+  static PrintWriter out=new PrintWriter( new OutputStreamWriter(System.out) );
+   
+  static int testCases,n;
+    
+    public static void main(String[] amit) throws IOException {
+        
+        testCases=in.nextInt();
+        
+        for(int t=0;t<testCases;t++){
+            
+            n=in.nextInt();
+            
+            int height=0;
+            
+            while(n>1){
+                
+                height++;
+                
+                int c=2;
+                
+                while(n>c-1){
+                    
+                    n-=c;
+                    
+                    c+=3;
+                    
+                }
+                
+            }
+            
+            out.println(height);
+            out.flush();
+            
+        }
+        
+    }
+    
+      static class Scanner{
+        
+        BufferedReader in;
+        StringTokenizer st;
+
+        public Scanner() {
+        
+            in=new BufferedReader( new InputStreamReader(System.in) );
+            
+        }
+        
+        String next() throws IOException{
+            
+            while(st==null || !st.hasMoreElements()){
+                
+                st=new StringTokenizer(in.readLine());
+                
+            }
+            
+            return st.nextToken();
+            
+        }
+        
+        int nextInt() throws IOException{
+            
+            return Integer.parseInt(next());
+            
+        }
+        
+        long nextLong() throws IOException{
+            
+            return Long.parseLong(next());
+            
+        }
+        
+        String nextLine() throws IOException{
+            
+            return in.readLine();
+            
+        }
+        
+        char nextChar() throws IOException{
+            
+            return next().charAt(0);
+            
+        }
+        
+        double nextDouble() throws IOException{
+            
+            return Double.parseDouble(next());
+            
+        }
+        
+        float nextFloat() throws IOException{
+            
+            return Float.parseFloat(next());
+            
+        }
+        
+        boolean nextBoolean() throws IOException{
+            
+            return Boolean.parseBoolean(next());
+            
+        }
+        
+        void close() throws IOException{
+            
+            in.close();
+            
+        }
+        
+    }
+    
+}
+/*
+
+5
+3
+14
+15
+24
+1
+
+
+*/
