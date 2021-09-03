4e22e9085ed65d6797dfb81847818ca5942fcadb
AmitRoy3370 <70816613+AmitRoy3370@users.noreply.github.com>
2021-09-03 09:32:28 +0600

    Add files via upload

---
+++ B_Maximum_Product.java
@@ -0,0 +1,249 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class B_Maximum_Product {
+
+     static Scanner in=new Scanner();
+    
+  static PrintWriter out=new PrintWriter( new OutputStreamWriter(System.out) );
+   
+  static int testCases,n;
+    
+  static long a[];
+  
+ static void merge(long a[],int left,int right,int mid){
+      
+      int n1=mid-left+1,n2=right-mid;
+      
+      long L[]=new long[n1];
+      long R[]=new long[n2];
+      
+      for(int i=0;i<n1;i++){
+          
+          L[i]=a[left+i];
+          
+      }
+      
+      for(int i=0;i<n2;i++){
+          
+          R[i]=a[mid+1+i];
+          
+      }
+      
+      int i=0,j=0,k=left;
+      
+      while(i<n1 && j<n2){
+          
+          if(L[i]<=R[j]){
+              
+              a[k]=L[i];
+              
+              i++;
+              
+          }else{
+              
+              a[k]=R[j];
+              
+              j++;
+              
+          }
+          
+          k++;
+          
+      }
+      
+      while(i<n1){
+          
+          a[k]=L[i];
+          
+          i++;
+          k++;
+          
+      }
+      
+      while(j<n2){
+          
+          a[k]=R[j];
+          
+          j++;
+          k++;
+          
+      }
+      
+  }
+  
+  static void sort(long a[],int left,int right){
+      
+      if(left>=right){
+          
+          return;
+          
+      }
+      
+      int mid=(left+right)/2;
+      
+      sort(a,left,mid);
+      sort(a,mid+1,right);
+      
+      merge(a,left,right,mid);
+      
+  }
+  
+  static void solve(){
+      
+      sort(a,0,n-1);
+      
+    /*  for(int i: a){
+          
+          out.print(i+" ");
+          out.flush();
+          
+      }
+      
+      out.println();
+      out.flush();*/
+      
+      long max=Long.MIN_VALUE;
+      
+      /*for(int i=0;i<5;i++){
+          
+          max*=a[i];
+          
+      }*/
+      
+      long a1=a[n-1]*a[n-2]*a[n-3]*a[n-4]*a[n-5];
+          long b=a[0]*a[1]*a[n-1]*a[n-2]*a[n-3];
+          long c=a[0]*a[1]*a[2]*a[3]*a[n-1];
+         
+          max=Math.max(max,Math.max( a1,Math.max(b,c) ));
+          
+      out.println(max);
+      out.flush();
+          
+  }
+  
+    public static void main(String[] amit) throws IOException {
+        
+      testCases=in.nextInt();
+        
+      for(int t=0;t<testCases;t++){
+          
+          n=in.nextInt();
+          
+          a=new long[n];
+          
+          for(int i=0;i<n;i++){
+              
+              a[i]=in.nextLong();
+              
+          }
+          
+          solve();
+          
+      }
+      
+      in.close();
+      
+    }
+    
+       static class Scanner{
+        
+        BufferedReader in;
+        StringTokenizer st;
+
+        public Scanner() {
+        
+            in=new BufferedReader( new InputStreamReader(System.in) );
+            
+        }
+        
+        String next() throws IOException{
+            
+            while(st==null || !st.hasMoreElements()){
+                
+                st=new StringTokenizer(in.readLine());
+                
+            }
+            
+            return st.nextToken();
+            
+        }
+        
+        int nextInt() throws IOException{
+            
+            return Integer.parseInt(next());
+            
+        }
+        
+        long nextLong() throws IOException{
+            
+            return Long.parseLong(next());
+            
+        }
+        
+        String nextLine() throws IOException{
+            
+            return in.readLine();
+            
+        }
+        
+        char nextChar() throws IOException{
+            
+            return next().charAt(0);
+            
+        }
+        
+        double nextDouble() throws IOException{
+            
+            return Double.parseDouble(next());
+            
+        }
+        
+        float nextFloat() throws IOException{
+            
+            return Float.parseFloat(next());
+            
+        }
+        
+        boolean nextBoolean() throws IOException{
+            
+            return Boolean.parseBoolean(next());
+            
+        }
+        
+        void close() throws IOException{
+            
+            in.close();
+            
+        }
+        
+    }
+    
+}
+/*
+
+4
+5
+-1 -2 -3 -4 -5
+6
+-1 -2 -3 1 2 -1
+6
+-1 0 0 0 -1 -1
+6
+-9 -7 -5 -3 -2 1
+
+
+*/
+
+/*
+
+1
+6
+-1 -2 -3 1 2 -1
+
+*/
