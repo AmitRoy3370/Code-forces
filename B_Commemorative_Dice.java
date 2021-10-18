a9eaad9a76b78bf796cd82e1cb89c8cb17bac221
AmitRoy3370 <70816613+AmitRoy3370@users.noreply.github.com>
2021-10-18 14:30:08 +0600

    Add files via upload

---
+++ B_Commemorative_Dice.java
@@ -0,0 +1,219 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class B_Commemorative_Dice{
+	
+	 static Scanner in=new Scanner();
+    
+    static PrintWriter out=new PrintWriter( new OutputStreamWriter(System.out) );
+
+	public static void main(String [] amit) throws IOException{
+		
+		long a[]=new long[7];
+		
+		long b[]=new long[7];
+		
+		int n=6;
+		
+		for(int i=1;i<=n;i++){
+			
+			a[i]=in.nextLong();
+			
+		}
+		
+		for(int i=1;i<=n;i++){
+			
+			b[i]=in.nextLong();
+			
+		}
+		
+		long ans=0;
+		
+		for(int i=1;i<=n;i++){
+			
+			for(int j=1;j<=n;j++){
+			
+			if(a[i]>b[j]){
+				
+				ans++;
+				
+			}
+			
+			}
+			
+		}
+		
+		long k=gcd( ans,36 );
+		
+		out.println( (ans/k)+"/"+(36/k) );
+		out.flush();
+		
+	}
+	
+	static long gcd(long a, long b)
+    {
+      if (b == 0)
+        return a;
+      return gcd(b, a % b);
+    }
+	
+   static void merge(long a[],int left,int right,int mid){
+        
+        int n1=mid-left+1,n2=right-mid;
+        
+        long L[]=new long[n1];
+        
+        long R[]=new long[n2];
+        
+        for(int i=0;i<n1;i++){
+            
+            L[i]=a[left+i];
+            
+        }
+        
+        for(int i=0;i<n2;i++){
+            
+            R[i]=a[mid+1+i];
+            
+        }
+        
+        int i=0,j=0,k1=left;
+        
+        while(i<n1 && j<n2){
+            
+            if( L[i]<=R[j] ){
+                
+                a[k1]=L[i];
+                
+                i++;
+                
+            }else{
+                
+                a[k1]=R[j];
+                
+                j++;
+                
+            }
+            
+            k1++;
+            
+        }
+        
+        while(i<n1){
+            
+            a[k1]=L[i];
+            
+            i++;
+            
+            k1++;
+            
+        }
+        
+        while(j<n2){
+            
+            a[k1]=R[j];
+            
+            j++;
+            k1++;
+            
+        }
+        
+    }
+    
+    static void sort(long a[],int left,int right){
+        
+        if(left>=right){
+            
+            return;
+            
+        }
+        
+        int mid=(left+right)/2;
+        
+        sort(a,left,mid);
+        
+        sort(a,mid+1,right);
+        
+        merge(a,left,right,mid);
+        
+    }
+	
+	  static class Scanner{
+        
+        BufferedReader in;
+        StringTokenizer st;
+
+        public Scanner() {
+        
+            in=new BufferedReader( new InputStreamReader(System.in) );
+            
+        }
+        
+        String next() throws IOException{
+            
+            while(st==null || !st.hasMoreElements()){
+                
+                st=new StringTokenizer(in.readLine());
+                
+            }
+            
+            return st.nextToken();
+            
+        }
+        
+        int nextInt() throws IOException{
+            
+            return Integer.parseInt(next());
+            
+        }
+        
+        long nextLong() throws IOException{
+            
+            return Long.parseLong(next());
+            
+        }
+        
+        String nextLine() throws IOException{
+            
+            return in.readLine();
+            
+        }
+        
+        char nextChar() throws IOException{
+            
+            return next().charAt(0);
+            
+        }
+        
+        double nextDouble() throws IOException{
+            
+            return Double.parseDouble(next());
+            
+        }
+        
+        float nextFloat() throws IOException{
+            
+            return Float.parseFloat(next());
+            
+        }
+        
+        boolean nextBoolean() throws IOException{
+            
+            return Boolean.parseBoolean(next());
+            
+        }
+        
+		
+        void close() throws IOException{
+            
+            in.close();
+            
+        }
+        
+    }
+	
+}
