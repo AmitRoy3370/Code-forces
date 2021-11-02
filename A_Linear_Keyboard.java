b6f1c4fef7c3a8bba5835580236f4931e1712596
AmitRoy3370 <70816613+AmitRoy3370@users.noreply.github.com>
2021-11-02 23:33:27 +0600

    Add files via upload

---
+++ A_Linear_Keyboard.java
@@ -0,0 +1,269 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.util.Map;
+import java.util.HashMap;
+
+public class A_Linear_Keyboard {
+
+    static Scanner in=new Scanner();
+    
+    static PrintWriter out=new PrintWriter( new OutputStreamWriter(System.out) );
+    
+    static int testCases,n,m;
+    
+    static char a[],s[];
+    
+    static void solve(){
+        
+        if(n==1){
+            
+            out.println(0);
+            out.flush();
+            
+            return;
+            
+        }
+        
+        Map<Character,Integer> map=new HashMap<>();
+        
+        for(int i=1;i<=m;i++){
+            
+            map.put(s[i-1],i  );
+            
+        }
+        
+       /* map.keySet().stream().map((i) -> {
+            out.println(i+" "+map.get(i));
+            return i;
+        }).forEachOrdered((_item) -> {
+            out.flush();
+        });
+        
+        out.println();
+        out.flush();*/
+        
+        long ans=0;
+        
+        for(int i=0;i<n-1;i++){
+            
+            ans+=Math.abs( map.get(a[i])-map.get(a[i+1]) );
+            
+        }
+        
+        out.println(ans);
+        out.flush();
+        
+    }
+    
+    public static void main(String[] amit) throws IOException {
+        
+        testCases=in.nextInt();
+        
+        for(int t=0;t<testCases;t++){
+            
+            s=in.next().toCharArray();
+            
+            a=in.next().toCharArray();
+            
+            n=a.length;
+            
+            m=s.length;
+            
+            solve();
+            
+        }
+        
+        in.close();
+        
+    }
+             
+    static long gcd(long a, long b)
+    {
+      if (b == 0)
+        return a;
+      return gcd(b, a % b);
+    }
+	
+   static void merge(long a[],int left,int right,int mid){
+        
+        int n1=mid-left+1,n2=right-mid;
+        
+        long L[]=new long[n1];
+        
+        long R[]=new long[n2];
+        
+        for(int i=0;i<n1;i++){
+            
+            L[i]=a[left+i];
+            
+        }
+        
+        for(int i=0;i<n2;i++){
+            
+            R[i]=a[mid+1+i];
+            
+        }
+        
+        int i=0,j=0,k1=left;
+        
+        while(i<n1 && j<n2){
+            
+            if( L[i]<=R[j] ){
+                
+                a[k1]=L[i];
+                
+                i++;
+                
+            }else{
+                
+                a[k1]=R[j];
+                
+                j++;
+                
+            }
+            
+            k1++;
+            
+        }
+        
+        while(i<n1){
+            
+            a[k1]=L[i];
+            
+            i++;
+            
+            k1++;
+            
+        }
+        
+        while(j<n2){
+            
+            a[k1]=R[j];
+            
+            j++;
+            k1++;
+            
+        }
+        
+    }
+    
+    static void sort(long a[],int left,int right){
+        
+        if(left>=right){
+            
+            return;
+            
+        }
+        
+        int mid=(left+right)/2;
+        
+        sort(a,left,mid);
+        
+        sort(a,mid+1,right);
+        
+        merge(a,left,right,mid);
+        
+    }
+	
+	  static class Scanner{
+        
+        BufferedReader in;
+        StringTokenizer st;
+
+        public Scanner() {
+        
+            in=new BufferedReader( new InputStreamReader(System.in) );
+            
+        }
+        
+        String next() throws IOException{
+            
+            while(st==null || !st.hasMoreElements()){
+                
+                st=new StringTokenizer(in.readLine());
+                
+            }
+            
+            return st.nextToken();
+            
+        }
+        
+        int nextInt() throws IOException{
+            
+            return Integer.parseInt(next());
+            
+        }
+        
+        long nextLong() throws IOException{
+            
+            return Long.parseLong(next());
+            
+        }
+        
+        String nextLine() throws IOException{
+            
+            return in.readLine();
+            
+        }
+        
+        char nextChar() throws IOException{
+            
+            return next().charAt(0);
+            
+        }
+        
+        double nextDouble() throws IOException{
+            
+            return Double.parseDouble(next());
+            
+        }
+        
+        float nextFloat() throws IOException{
+            
+            return Float.parseFloat(next());
+            
+        }
+        
+        boolean nextBoolean() throws IOException{
+            
+            return Boolean.parseBoolean(next());
+            
+        }
+        
+        void close() throws IOException{
+            
+            in.close();
+            
+        }
+        
+    }
+	
+}
+/*
+
+5
+abcdefghijklmnopqrstuvwxyz
+hello
+abcdefghijklmnopqrstuvwxyz
+i
+abcdefghijklmnopqrstuvwxyz
+codeforces
+qwertyuiopasdfghjklzxcvbnm
+qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
+qwertyuiopasdfghjklzxcvbnm
+abacaba
+
+
+*/
+
+/*
+
+1
+abcdefghijklmnopqrstuvwxyz
+codeforces
+
+*/
